060f620c51e8a930d43bc33dfa06862329b1267c
lukvmil <millerluke1364@gmail.com>
2022-08-16 21:04:14 +0000

    restructured static files, transition to a dynamic loading system using carousels for viewing posts. better for mobile, doesn't reload already seen images

---
+++ static/js/add-entry.js
@@ -0,0 +1,139 @@
+const imageInput = document.getElementById("image-input");
+const imageDisplay = document.getElementById("image-display");
+const imageContainer = document.getElementById("image-container")
+const textInput = document.getElementById("text-input");
+const coordinateDisplay = document.getElementById("coordinate-display")
+const validationMessage = document.getElementById("validation-message");
+const submitButton = document.getElementById("submit-button");
+const loadingIcon = document.getElementById("loading-icon");
+const params = new URLSearchParams(window.location.search);
+const locationErrorModal = new bootstrap.Modal(document.getElementById('location-error'))
+
+let raw_geoloc;
+let geoloc;
+
+coordinateDisplay.value = "";
+
+function triggerUpload() {
+    imageInput.click();
+}
+
+function loadImage() {
+    imageDisplay.src = URL.createObjectURL(imageInput.files[0]);
+}
+
+function requestLocation() {
+    navigator.geolocation.getCurrentPosition(handleLocationSuccess, handleLocationFail);
+}
+
+function handleLocationSuccess(loc) {
+    raw_geoloc = {
+        "latitude": loc.coords.latitude,
+        "longitude": loc.coords.longitude,
+        "timestamp": loc.timestamp
+    }
+
+    handlePrecision(true);
+}
+
+function roundTo(number, places) {
+    factor = 10 ** places;
+    return Math.round(number * factor) / factor; 
+}
+
+function handlePrecision(enabled) {
+    let lon, lat;
+
+    if (enabled) {
+        lon = roundTo(raw_geoloc.longitude, 5);
+        lat = roundTo(raw_geoloc.latitude, 5);
+    } else {
+        lon = roundTo(raw_geoloc.longitude, 2) + Math.floor(Math.random() * 10) / 10 ** 3;
+        lat = roundTo(raw_geoloc.latitude, 2) + Math.floor(Math.random() * 10) / 10 ** 3;
+    }
+
+    geoloc = {
+        "latitude": lat,
+        "longitude": lon,
+        "timestamp": raw_geoloc.timestamp
+    }
+
+    coordinateDisplay.value = `${lat}, ${lon}`;
+}
+
+function handleLocationFail(error) {
+    error.code;
+    error.message;
+
+    locationErrorModal.show();
+}
+
+function validateItem(condition, element) {
+    if (condition) {
+        element.classList.remove("border-danger");
+        element.classList.add("border-success");
+        return true;
+    } else {
+        element.classList.remove("border-success");
+        element.classList.add("border-danger");
+        return false;
+    }
+}
+
+function postEntry() {
+    let data = new FormData();
+    data.append('file', imageInput.files[0]);
+    data.append('geoloc', JSON.stringify(geoloc));
+    data.append('message', textInput.value);
+    data.append('box_key', sessionStorage.getItem("box_key"));
+
+    let box_id = params.get("id");
+
+    console.log(geoloc);
+
+    console.log("submitted...")
+
+    fetch(`api/box/${box_id}/entry`, {
+        method: 'POST',
+        body: data
+    })
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data);
+            location.href = `box?id=${box_id}`
+        })
+}
+
+function submitEntry() {
+    let imageValid = validateItem(geoloc, coordinateDisplay);
+    let locationValid = validateItem(textInput.value, textInput);
+    let messageValid = validateItem(imageInput.files[0], imageContainer);
+
+    validationMessage.removeAttribute("hidden");
+
+    if (imageValid && locationValid && messageValid) {
+        validationMessage.classList.remove("text-danger");
+        validationMessage.classList.add("text-success");
+        validationMessage.innerText = "Looks good!";
+        loadingIcon.removeAttribute("hidden");
+        postEntry();
+    } else {
+        validationMessage.classList.remove("text-success");
+        validationMessage.classList.add("text-danger");
+        validationMessage.innerText = "Make sure you complete all items!";
+    }
+}
+
+if (imageInput.files[0]) {
+    loadImage();
+}
+
+if (params.has("key")) {
+    let box_key = params.get("key");
+    sessionStorage.setItem("box_key", box_key);
+    fetch(`api/get_id?key=${box_key}`)
+        .then(data => data.json())
+        .then(data => {
+            window.location.replace(`${window.location.pathname}?id=${data.pub_id}`);
+        })
+}
---
+++ static/js/box.js
@@ -0,0 +1,241 @@
+const params = new URLSearchParams(window.location.search);
+const addEntryButton = document.getElementById("add-entry");
+const entryBody = document.getElementById("entry-body");
+const entryTimestamp = document.getElementById("entry-timestamp");
+const entryLocation = document.getElementById("entry-location");
+const entryImages = document.getElementById("entry-images");
+const entryControls = document.getElementById("entryControls");
+const entryCarousel= new bootstrap.Carousel(entryControls);
+
+const loadProximity = 1;
+
+let pointSource;
+let lineSource;
+let markerLayer;
+let dotLayer;
+let lineLayer;
+let activeEntryId;
+
+const coords = []
+const entryList = []
+
+
+function generateCarouselItems(count) {
+    function createCarouselItem() {
+        let img = document.createElement('img');
+        img.className = 'd-block w-100 card-img-bottom active';
+        let div = document.createElement('div');
+        div.className = "carousel-item";
+        div.appendChild(img);
+        return div;
+    }
+
+    let firstItem = document.getElementById('initial-entry-image')
+    for (let i = 0; i < count - 1; i++) {
+        entryImages.insertBefore(createCarouselItem(), firstItem);
+    }
+}
+
+function loadEntryImage(id) {
+    let entryImage = entryImages.children[id];
+    if (entryImage.loaded) {return}
+    entryImage.children[0].src = `api/img/${entryList[id].image}`;
+    entryImage.loaded = true;
+    console.log("loaded image " + id);
+}
+
+function loadProximateImages(id) {
+    let proximateImages = [id];
+    let max = entryList.length - 1;
+    let min = 0;
+    let currentId = id;
+
+    for (let i = 1; i <= loadProximity; i++) {
+        let nextId = currentId - 1;
+        if (nextId < min) {nextId = max;}
+        currentId = nextId;
+        proximateImages.push(currentId);
+    }
+
+    currentId = id;
+
+    for (let i = 1; i <= loadProximity; i++) {
+        let nextId = currentId + 1;
+        if (nextId > max) {nextId = min;}
+        currentId = nextId;
+        proximateImages.push(currentId);
+    }
+
+    proximateImages.forEach(id => loadEntryImage(id));
+}
+
+function setActiveEntry(id) {
+    activeEntryId = id;
+    e = entryList[activeEntryId];
+    if (!e) {return}
+    entryBody.innerText = e.message;
+    entryTimestamp.innerText = new Date(e.timestamp).toLocaleString();
+    entryLocation.innerText = e.location_str;
+    // entryImage.setAttribute("src", `api/img/${e.image}`);
+}
+
+if (params.has("k")) {
+    let box_key = params.get("k");
+    sessionStorage.setItem("box_key", box_key);
+    fetch(`api/get_id?key=${box_key}`)
+        .then(resp => resp.json())
+        .then(data => {
+            window.location.href = `${window.location.pathname}?id=${data.id}`;
+        })
+}
+
+if (sessionStorage.getItem("box_key")) {
+    addEntryButton.removeAttribute("hidden");
+}
+
+if (params.has("id")) {
+    let box_id = params.get("id");
+    fetch(`api/box/${box_id}/entries`)
+        .then(resp => {
+            if (resp.status == 200) {
+                return resp.json();
+            } else {
+                
+            }
+        })
+        .then(entries => {
+            console.log(`Loaded ${entries.length} entries`)
+            generateCarouselItems(entries.length);
+            entries.forEach((e, i) => {
+                entryList.push(e);
+                coords.push(e.location.reverse());
+            });
+            loadProximateImages(entries.length - 1);
+            setActiveEntry(entries.length - 1);
+            if (entries.length) {
+                loadLines();
+                loadPoints();
+            }
+            // entryCarousel.to(0);
+        });
+}
+
+
+entryControls.addEventListener("slide.bs.carousel", event => {
+    console.log(event.to);
+    loadProximateImages(event.to);
+})
+
+
+let map = new ol.Map({
+    target: 'map',
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([-95, 37]),
+        zoom: 3,
+        maxZoom: 16
+    })
+
+});
+
+const markerStyle = new ol.style.Style({
+    image: new ol.style.Circle({
+        radius: 10,
+        fill: new ol.style.Fill({
+            color: '#fff',
+        }),
+        stroke: new ol.style.Stroke({
+            color: '#212529',
+            width: 4,
+        }),
+    }),
+});
+
+const dotStyle = new ol.style.Style({
+    image: new ol.style.Circle({
+        radius: 4,
+        fill: new ol.style.Fill({
+            color: '#000',
+        })
+    })
+});
+
+const lineStyle = new ol.style.Style({
+    stroke: new ol.style.Stroke({
+        color: "#212529",
+        width: 4
+    })
+});
+
+const lineFeatures = [];
+const pointFeatures = [];
+
+function loadPoints() {
+    let count = 0;
+    coords.forEach(c => {
+        pointFeatures.push(
+            new ol.Feature({
+                geometry: new ol.geom.Point(
+                    ol.proj.fromLonLat(c)
+                ),
+                name: count
+            })
+        );
+        count += 1;
+    });
+    pointSource = new ol.source.Vector({ features: pointFeatures });
+    map.getView().fit(pointSource.getExtent(), {padding: [100, 100, 100, 100], maxZoom: 14});
+    markerLayer = new ol.layer.Vector({
+        source: pointSource,
+        style: markerStyle
+    });
+
+    dotLayer = new ol.layer.Vector({
+        source: pointSource,
+        style: dotStyle
+    });
+    map.addLayer(markerLayer);
+    // map.addLayer(dotLayer);
+    let select = new ol.interaction.Select({
+        condition: ol.events.condition.click,
+        layers: [markerLayer],
+        style: dotStyle
+    });
+
+    map.addInteraction(select);
+
+    select.on('select', e => {
+        let features = e.target.getFeatures().getArray()
+        if (features.length) {
+            let name = features[0].get('name')
+            console.log(name);
+            setActiveEntry(name);
+        }
+    });
+}
+
+
+
+function loadLines() {
+    for (let i = 0; i < coords.length - 1; i++) {
+        lineFeatures.push(
+            new ol.Feature({
+                geometry: new ol.geom.LineString([
+                    ol.proj.fromLonLat(coords[i]),
+                    ol.proj.fromLonLat(coords[i + 1])
+                ]),
+            })
+        );
+    }
+    lineSource = new ol.source.Vector({ features: lineFeatures });
+    lineLayer = new ol.layer.Vector({
+        source: lineSource,
+        style: lineStyle
+    });
+    map.addLayer(lineLayer);
+}
+
