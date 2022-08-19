0fa0225e6dff43b72012b20dc9b7a5b3972f3afd
lukvmil <millerluke1364@gmail.com>
2022-08-19 13:35:31 +0000

    more functionality to manage tool

--- tools/manage.py
+++ tools/manage.py
@@ -4,27 +4,80 @@ def show_dash():
     total_boxes = len(BoxModel.objects())
     active_boxes = len(BoxModel.objects(active=True))
     total_entries = len(EntryModel.objects())
-    print("BoxQuest Management Dashboard")
+    print("[BoxQuest Management Dashboard]")
     print(f"{active_boxes}/{total_boxes} active boxes, {total_entries} entries")
-    print("[1] View Box")
-    print("[2] Delete Box")
+    print("[1] Active boxes")
+    print("[2] Box by ID")
     print("[Q] Quit")
+    print()
 
 def get_box(_id):
     return BoxModel.objects(pub_id=_id).first()
 
+def get_entry(_id):
+    return EntryModel.objects(id=_id).first()
+
+def show_entry(box, _id):
+    entry = get_entry(_id)
+    if not entry:
+        print("Invalid ID")
+        print()
+        return False
+
+    while True:
+        print(f"[Entry {entry.id}]")
+        print("Timestamp:", entry.timestamp)
+        print("Location:", entry.location)
+        print("Location String:", entry.location_str)
+        print("Message:", entry.message)
+        print("Image:", entry.image.grid_id)
+        print()
+        print("[1] Delete")
+        print()
+        sel = input("> ")
+        
+        if sel == "1":
+            print("Are you sure?")
+            print()
+            sel = input("> ")
+            if sel == "y":
+                box.update(pull__entries=entry)
+                entry.delete()
+                print("Deleted")
+                break
+        
+        if not sel: break
+
 def show_box(_id):
-    box = get_box()
+    box = get_box(_id)
     if not box:
         print("Invalid ID")
+        print()
         return False
     
-    print("Key:", box.key)
-    print("Active:", box.active)
-    print("Quest:", box.quest)
-    print("Guide:", box.guide)
-    print("Public:", box.public)
-    print("Entries:", len(box.entries))
+    while True:
+        print(f"[Box {box.id}]")
+        print("Key:", box.key)
+        print("Active:", box.active)
+        print("Quest:", box.quest)
+        print("Guide:", box.guide)
+        print("Public:", box.public)
+        print("Entries:", len(box.entries))
+        print()
+        print("[1] View entries")
+        print("[2] Delete")
+        print()
+        sel = input("> ")
+
+        if sel == "1":
+            print(f"[Entries for box {box.id}]")
+            for i, e in enumerate(box.entries):
+                print(f"[{i+1}] {e.id}")
+            print()
+            sel = input("> ")
+            if not sel: continue
+            show_entry(box, box.entries[int(sel)-1].id)
+        else: break
     return True
 
 while True:
@@ -32,12 +85,24 @@ while True:
     sel = input("> ")
 
     if sel == "1":
-        print("Box ID?")
+        active_boxes = BoxModel.objects(active=True)
+        print("[Active boxes]")
+        for i, a in enumerate(active_boxes):
+            print(f"[{i+1}] {a.id}")
+        print()
         sel = input("> ")
-        show_box(sel)
+        if not sel: continue
+        show_box(active_boxes[int(sel)-1].id)
 
     elif sel == "2":
         print("Box ID?")
+        print()
+        sel = input("> ")
+        show_box(sel)
+
+    elif sel == "3":
+        print("Box ID?")
+        print()
         sel = input("> ")
         box = get_box(sel)
         if not box: print("Invalid ID"); continue
